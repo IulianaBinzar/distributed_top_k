8ac99ca57b44b164d105045d0faca869f9cd0e11
Iuliana Binzar <iuliabinzar@gmail.com>
2024-11-23 21:09:11 +0000

    Added a stream forwarder

---
+++ StreamForwarder.py
@@ -0,0 +1,11 @@
+class StreamForwarder:
+    def __init__(self, sites):
+        self.sites = sites
+
+    def forward_to_site(self, log_file):
+        for log_line in log_file:
+            # log_format "timestamp url log_site"
+            log_site = log_line.strip().split(" ")[2]
+            if log_site in range(0, 3):
+                self.sites[log_site].get_log(log_line)
+            # silently discarding logs that have invalid site codes
