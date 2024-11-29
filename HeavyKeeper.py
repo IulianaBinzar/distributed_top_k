dc4358938707ff468803f81eb8093591640700ca
Iuliana Binzar <iuliabinzar@gmail.com>
2024-11-29 16:02:30 +0000

    Updated HeavyKeeper

--- HeavyKeeper.py
+++ HeavyKeeper.py
@@ -1,4 +1,5 @@
 from crypthography.Fernet import Fernet
+from collections import defaultdict
 
 class HeavyKeeper:
     def __init__(self, k):
@@ -14,18 +15,19 @@ class HeavyKeeper:
         self.hash_size = 5000
         # Other initialisations
         self.k = k # length of the list being kept
-        self.sketch = [{} for x in range(len(self.hash_algs))]
+        self.sketch = [[(None, 0)
+                        for _ in range(self.hash_size)]
+                       for _ in range(self.hash_keys)]
         self.current_top_k = []
 
-    def get_hashed_top_k(self) -> List[int]:
+    def get_hashed_top_k(self) -> List[bytes]:
         hashed_result = list(self.current_top_k)
         hashed_result.sort(reverse=True)
         return hashed_result
 
-    def get_string_top_k(self, hash_key: bytes) -> List[int]:
+    def get_string_top_k(self, hash_key: bytes) -> List[str]:
+        hashed_result = self.get_hashed_top_k()
         fernet = Fernet(hash_key)
-        hashed_result = list(self.current_top_k)
-        hashed_result.sort(reverse=True)
         string_result = [fernet.decrypt(x) for x in hashed_result]
         return string_result
 
@@ -36,5 +38,19 @@ class HeavyKeeper:
         return encrypted_url
 
     def process_log(self, accesed_url: str):
+        true_count = 0
 
+        for i, x in enumerate(self.hash_keys):
+            fingerprint = self.url_fingerprint(accesed_url, x)
+            hash_index = fingerprint % self.hash_size
+            sketch_fp, sketch_counter = self.sketch[i][hash_index]
+            if not sketch_counter:
+                self.sketch[i][hash_index] = (fingerprint, 1)
+                true_count = min(true_count, 1)
+            if sketch_fp == fingerprint:
+                sketch_counter += 1
+                self.sketch[i][hash_index] = (fingerprint, sketch_counter)
+                true_count = min(true_count, sketch_counter)
+            else:
+                self.sketch[i][hash_index]["counter"] *= self.b
         return None
