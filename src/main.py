a7488750ac91beb8b50b61acee40e37c88d49ef6
Iuliana Binzar <iuliabinzar@gmail.com>
2024-11-30 17:02:44 +0000

    Restructured folders

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ def main():
     k = 10
     sites = {site_id: Site(site_id, k) for site_id in range(3)}
     stream_forwarder = StreamForwarder(sites)
-    with open('./input/mixed_wc_day51_3.log', 'r', encoding='ISO-8859-1') as stream:
+    with open('./../input/mixed_wc_day51_3.log', 'r', encoding='ISO-8859-1') as stream:
         logging.info("Parsing stream.")
         stream_forwarder.forward_to_site(stream)
 
