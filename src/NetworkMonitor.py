ac1e9760bf292d1450b153adf03a756b6647c48c
Iuliana Binzar <iuliabinzar@gmail.com>
2024-12-02 04:15:52 +0000

    Switching to computing correlations every time all the node_top_ks are filled

--- src/NetworkMonitor.py
+++ src/NetworkMonitor.py
@@ -4,45 +4,55 @@ from datetime import timedelta
 import pandas as pd
 
 class NetworkMonitor:
-    def __init__(self, k: int):
+    def __init__(self, k: int, node_count: int):
         self.k = k
         self.node_top_k = {}
-        self.last_aggregate_time = None
+        self.node_count = node_count
+        self.latest_data_collected = [False for _ in range(self.node_count)]
 
     def receive_top_k(self, node_id, top_k, timestamp):
         """
          Converts top_k lists to panda dataframes for further processing
         """
-        logging.debug(f"Network Monitor received top-k for node {node_id} at timestamp {timestamp}: {top_k}")
-        if node_id not in self.node_top_k:
-            self.node_top_k[node_id] = pd.DataFrame(columns=["timestamp",
-                                                             "item",
-                                                             "frequency"])
+        logging.info(f"Network Monitor received top-k for node {node_id} at timestamp {timestamp}: {top_k}")
+        top_k_dataframe = pd.DataFrame(columns=["timestamp",
+                                                "url",
+                                                "frequency"])
         for freq, item in top_k:
-            self.node_top_k[node_id] = pd.concat([
-                self.node_top_k[node_id],
+            top_k_dataframe = pd.concat([
+                top_k_dataframe,
                 pd.DataFrame({"timestamp": [timestamp],
-                              "item": [item],
+                              "url": [item],
                               "frequency": [freq]})
             ], ignore_index=True)
 
-        logging.debug(f"Node {node_id} dataframe: \n{self.node_top_k[node_id]}")
+        self.node_top_k[node_id] = top_k_dataframe
+        self.latest_data_collected[node_id] = True
+        logging.info(f"Node {node_id} dataframe: \n{self.node_top_k[node_id]}")
 
-        if not self.last_aggregate_time:
-            self.last_aggregate_time = timestamp
+        # if not self.last_aggregate_time:
+        #     self.last_aggregate_time = timestamp
+        #
+        # if timestamp - self.last_aggregate_time > timedelta(minutes=10):
+        #     self.correlate_node_results()
+        #     self.last_aggregate_time = timestamp
 
-        if timestamp - self.last_aggregate_time > timedelta(minutes=10):
-            self.correlate_node_results()
-            self.last_aggregate_time = timestamp
-
-
-    def correlate_node_results(self):
-        global_dataframe = pd.concat(self.node_top_k, keys=self.node_top_k.keys(), names=["site_id", "row_index"])
-        global_dataframe.reset_index(level="row_index", drop=True, inplace=True)
-        global_dataframe = global_dataframe.pivot_table(index="timestamp", columns="site_id", values="frequency", aggfunc="sum")
-        global_dataframe = global_dataframe.fillna(0)
-        correlation_matrix = global_dataframe.corr()
-        logging.info(f"Combined data \n {global_dataframe}")
-        logging.info(f"Correlation matrix \n {correlation_matrix}")
-        return correlation_matrix
+    #     if all(self.latest_data_collected):
+    #         self.correlate_node_results()
+    #         self.latest_data_collected = [False for _ in range(self.node_count)]
+    #
+    #
+    # def correlate_node_results(self):
+    #     unique_urls = set()
+    #     for x, dataframe in self.node_top_k.items():
+    #         unique_urls.update(dataframe["url"].unique())
+    #     unique_urls = sorted(unique_urls)
+    #     ranked_dataframe = pd.DataFrame(index=unique_urls)
+    #     for site_id, df in self.node_top_k.items():
+    #         ranked_urls = df.sort_values(by="frequency", ascending=False)["url"].reset_index(drop=True)
+    #         rank_dict = {url: rank + 1 for rank, url in enumerate(ranked_urls)}
+    #         ranked_dataframe[f"Node_{site_id}"] = ranked_dataframe.index.map(rank_dict)
+    #     ranked_dataframe = ranked_dataframe.fillna(0)
+    #     correlation_matrix = ranked_dataframe.corr(method='spearman')
+    #     return correlation_matrix
 
--- test/Test_NetworkMonitor.py
+++ test/Test_NetworkMonitor.py
@@ -6,13 +6,19 @@ from NetworkMonitor import NetworkMonitor
 
 class TestNetworkMonitor(unittest.TestCase):
     def test_receive_top_k(self):
-        network_monitor = NetworkMonitor(5)
-        top_k_site1 = [(9, "a"), (8, "b"), (4, "c")]
-        top_k_site2 = [(9, "a"), (8, "b"), (4, "c")]
+        network_monitor = NetworkMonitor(5, 3)
+        top_k_site12 = [(9, "a"), (8, "b"), (4, "c")]
         top_k_site3 = [(3, "j"), (2, "i"), (1, "x")]
-        network_monitor.receive_top_k(0, top_k_site1, datetime.now())
-        network_monitor.receive_top_k(0, top_k_site2, datetime.now())
-        network_monitor.receive_top_k(0, top_k_site3, datetime.now())
+        top_k_site12_2 = [(91, "a1"), (81, "b1"), (41, "c1")]
+        top_k_site3_2 = [(31, "j1"), (12, "i1"), (1, "x1")]
+        test_timestamp = datetime.now()
+        test_timestamp2 = datetime.now()
+        network_monitor.receive_top_k(0, top_k_site12, test_timestamp)
+        network_monitor.receive_top_k(1, top_k_site12, test_timestamp)
+        network_monitor.receive_top_k(2, top_k_site3, test_timestamp)
+        network_monitor.receive_top_k(0, top_k_site12_2, test_timestamp2)
+        network_monitor.receive_top_k(1, top_k_site3, test_timestamp2)
+        network_monitor.receive_top_k(2, top_k_site3_2, test_timestamp2)
         res_corr_matrix = network_monitor.correlate_node_results()
-        self.assertEqual(res_corr_matrix, None)
+        self.assertEqual(3, len(res_corr_matrix))
         # TODO fix this
