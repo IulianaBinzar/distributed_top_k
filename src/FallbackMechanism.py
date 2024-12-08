89a250ecc3eec5829d4ec42e3de1a6e248e12c47
Iuliana Binzar <iuliabinzar@gmail.com>
2024-12-08 11:36:57 +0000

    Fixed the mask tensor

--- src/FallbackMechanism.py
+++ src/FallbackMechanism.py
@@ -7,7 +7,7 @@ def pad_to_k_and_mask(lst, k):
     mask = [1 if x >= 0 else 0 for x in padded_list]
     return padded_list, mask
 
-def df_to_tensor(data_frame: pd.DataFrame, seq_len: int, node_count: int, k: int):
+def df_to_tensors(data_frame: pd.DataFrame, seq_len: int, node_count: int, k: int):
     # handling the case len(top_k) < k and creating a mask
     padded_data = data_frame.applymap(lambda x: pad_to_k_and_mask(x, k))
     padded_df = padded_data.applymap(lambda x: x[0])
@@ -17,7 +17,11 @@ def df_to_tensor(data_frame: pd.DataFrame, seq_len: int, node_count: int, k: int
     flattened_df = padded_df.applymap(lambda x: torch.tensor(x, dtype=torch.float32))
     flattened_df = flattened_df.values.flatten().tolist()
     input_tensor = torch.stack(flattened_df).view(-1, seq_len, node_count*k)
-    return input_tensor
+    # creating mask tensor
+    flattened_mask = mask_df.applymap(lambda x: torch.tensor(x, dtype=torch.float32))
+    flattened_mask = flattened_mask.values.flatten().tolist()
+    mask_tensor = torch.stack(flattened_mask).view(-1, seq_len, node_count*k)
+    return input_tensor, mask_tensor
 
 class FallbackMechanism(nn.Module):
     def __init__(self, input_size, output_size, hidden_size, num_layers, dropout):
@@ -32,7 +36,7 @@ class FallbackMechanism(nn.Module):
 
     def feed_data(self, data_frame, seq_len, node_count, k):
         data_frame = data_frame.drop(columns="timestamp")
-        tensor = df_to_tensor(data_frame, seq_len, node_count, k)
+        tensor, mask = df_to_tensors(data_frame, seq_len, node_count, k)
         self.forward(tensor)
 
     def forward(self, x):
