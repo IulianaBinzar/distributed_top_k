7f32125f2c3002340b898e2b6a481b6a58dbaa12
Iuliana Binzar <iuliabinzar@gmail.com>
2024-12-15 19:23:54 +0000

    Added a README file

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# distribted_top_k
+# An ML-Augmented HeavyKeeper-Based Fallback Mechanism for Top-K Estimation in Distributed Data Streams
+
+The project simulates a distributed system using the [1998 World Cup Website Access Logs](). It computes the top-k most 
+often accessed URLs for each site. Periodically a node is assumed to have failed. In that case, the fallback mechanism 
+is called to estimate the top-k based on the top-k of peer nodes and observed correlations between them. 
+
+To reduce computational overhead, we assume that node 0 is the failing node.
+
+## Input
+The datastream was preprocessed to retain only the following columns:
+```
+[Timestamp TimeDiff] URL SiteCode
+```
+Sample input:
+```
+[15/Jun/1998:10:53:39 +0000] /images/hm_score_up_line02.gif 1
+[15/Jun/1998:10:53:39 +0000] /english/index.html 2
+```
+Where the site codes are mapped as follows
+
+    Santa Clara 0
+    Plano 1
+    Herndon 2
+    Paris 3
+
+## Pipeline Overview 
+The preprocessed logs are parsed line by line in `main.py`. The log line is passed to `stream_forwarder.py` 
+where it is attributed to the right `site_processor` based on the code. Each site has an associated `heavy_keeper` instance. 
+Once the top-k is ready to be reported it is forwarded to the `NetworkMonitor`. The network monitor trains an instance of 
+`FallbackMechanism` and estimates the next top-k in `network_monitor.fallback_mechanism()`. 
+
+## Output
+The current setup returns the inferred top-k list and the actual list for comparison. For better visibility and reduced verbosity,
+the URLs are returned as an encoded list, but they can be decoded if necessary. 
+
+```
+2024-12-15 19:20:15,394 WARNING: Predicted Top-K URLs for Node 0:[[5, 4, 8, 3, 6, 9, 7, 1, 0, 2]]
+2024-12-15 19:20:15,396 WARNING: Actual Top-K URLs for Node 0:[[5, 4, 3, 2, 9, 7, 1, 4, 33, 10]]
+```
--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ def main():
     )
     node_count = 3
     # Node 3 is too sparsely populated, will have to change back to 4
-    k = 15
-    batch_size = 3
+    k = 10
+    batch_size = 4
     step_size = 1
 
     network_monitor = NetworkMonitor(k, node_count, batch_size)
@@ -34,15 +34,22 @@ def main():
                 if inference_due >= 10 * batch_size:
                     network_monitor.fallback_mechanism.eval()
                     with torch.no_grad():
-                        tensor, mask, _ = network_monitor.prepare_and_validate_tensor()
+                        tensor, mask, target_tensor = network_monitor.prepare_and_validate_tensor()
                         network_monitor.sliding_window_df = network_monitor.sliding_window_df[step_size:].reset_index(
                             drop=True)
                         predictions = network_monitor.fallback_mechanism(tensor)
-                        predicted_urls = [
-                            [network_monitor.id_to_url[x.item()] for x in pred]
-                            for pred in torch.topk(predictions, k=5, dim=-1).indices
-                        ]
-                        logging.warn(f"Predicted Top-K URLs for node 0:{predicted_urls}")
+                        # Decoded output
+                        # predicted_urls = [
+                        #     [network_monitor.id_to_url[x.item()] for x in pred]
+                        #     for pred in torch.topk(predictions, k, dim=-1).indices
+                        # ]
+                        # actual_urls = [
+                        #     [network_monitor.id_to_url[x.item()] for x in act]
+                        #     for act in torch.topk(target_tensor, k, dim=-1).indices
+                        # ]
+                        pred_id = [[x.item() for x in pred]for pred in torch.topk(predictions, k, dim=-1).indices]
+                    logging.warning(f"Predicted Top-K URLs for Node 0:{pred_id}")
+                    logging.warning(f"Actual Top-K URLs for Node 0:{target_tensor.to(torch.int).tolist()}")
                     inference_due = 0
                     continue
                 inference_due += 1
