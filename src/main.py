8e880fd65b1d8bf7188bf1643fd3378d6dac694c
Iuliana Binzar <iuliabinzar@gmail.com>
2025-03-02 14:47:43 +0000

    Parsing from S3

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import torch.optim
 import yaml
 import logging
+import boto3
+import io
 
 from network_monitor import NetworkMonitor
 from stream_forwarder import StreamForwarder
@@ -11,7 +13,7 @@ from utils.pipeline_utils import train_model
 
 def main():
     logging.basicConfig(
-        level=logging.WARN, format="%(asctime)s %(levelname)s: %(message)s"
+        level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s"
     )
     with open('../config.yaml', 'r') as conf_file:
         conf = yaml.safe_load(conf_file)
@@ -20,11 +22,20 @@ def main():
     node_count = conf['node_count']
     batch_size = conf['batch_size']
 
+    # List log files in S3 bucket
+    s3_bucket = "fifa-access-logs-25"
+    s3_client = boto3.client('s3')
+    response = s3_client.list_objects_v2(Bucket=s3_bucket)
+    log_files = [obj['Key'] for obj in response.get('Contents', []) if obj['Key'].endswith('.log')]
+    logging.info(F"Response: {response}")
+    logging.info(f"Found log files: {log_files}")
+
     network_monitor = NetworkMonitor(
         k,
         node_count,
         batch_size,
-        conf['step_size'])
+        conf['step_size']
+    )
 
     optimizer = torch.optim.AdamW(
         network_monitor.fallback_mechanism.parameters(),
@@ -37,20 +48,23 @@ def main():
     }
     stream_forwarder = StreamForwarder(sites)
 
-    # Workflow
-    with open("./../input/mixed_wc_day51_3.log", "r", encoding="ISO-8859-1") as stream:
-        logging.info("Parsing stream:")
+    # Process each log file from S3
+    for log_file in log_files:
+        logging.info(f"Processing {log_file} from S3")
+        s3_object = s3_client.get_object(Bucket=s3_bucket, Key=log_file)
+        log_stream = io.StringIO(s3_object['Body'].read().decode('ISO-8859-1'))
+
         inference_due = 0
-        for line_id, line in enumerate(stream):
+        for line_id, line in enumerate(log_stream):
             stream_forwarder.forward_to_site(line)
             if len(network_monitor.sliding_window_df) >= batch_size:
-                # Inference
                 if inference_due >= conf['inference_frequency'] * batch_size:
                     node_failure_simulation(network_monitor)
                     inference_due = 0
                     continue
                 inference_due += 1
                 train_model(network_monitor, optimizer, epochs=conf['epochs'])
+
 
 if __name__ == "__main__":
     main()
